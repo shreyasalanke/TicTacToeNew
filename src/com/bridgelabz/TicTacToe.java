bad5384f5cb0a5e2e4294eb6676bba4e4e7984d3
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-09 19:26:57 +0530

    ADDED:create a board for tic tac toe board display

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -6,8 +6,18 @@ public class TicTacToe
 	 public static char[] board=new char[10];
 	 public static void main(String[] args)
 	 {
-	        System.out.println("Welcome to tic tac toe game");
+	        System.out.println("Welcome to Tic Tac Toe Game");
 	        createBoard();
+	        displayBoard();
+	 }
+	 private static void displayBoard()
+	    {
+	        System.out.println("The Current Board is:");
+	        System.out.println("| " + board[1] + " | "+ board[2] + " | " + board[3]+ " |");
+	        System.out.println("|-----------|");
+	        System.out.println("| " + board[4] + " | "+ board[5] + " | " + board[6]+ " |");
+	        System.out.println("|-----------|");
+	        System.out.println("| " + board[7] + " | "+ board[8] + " | " + board[9]+ " |");
 	    }
 	    private static char[] createBoard()
 	    {
