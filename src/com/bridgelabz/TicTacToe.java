427a4a953bc2f013b6567b0381977ebefc69eae6
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-09 19:31:57 +0530

    ADDED:create a board for tic tac toe board game and enable user input

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -9,10 +9,10 @@ public class TicTacToe
 	        System.out.println("Welcome to Tic Tac Toe Game");
 	        createBoard();
 	        displayBoard();
+	        userOption();
 	 }
 	 private static void displayBoard()
 	    {
-	        System.out.println("The Current Board is:");
 	        System.out.println("| " + board[1] + " | "+ board[2] + " | " + board[3]+ " |");
 	        System.out.println("|-----------|");
 	        System.out.println("| " + board[4] + " | "+ board[5] + " | " + board[6]+ " |");
@@ -27,5 +27,27 @@ public class TicTacToe
 	            board[i]='-';
 	        }
 	        return board;
+	    }
+	    private static void userOption()
+	    {
+	        System.out.println("Choose between x or o : ");
+	        Scanner scanner = new Scanner(System.in);
+	        char userTurn=scanner.next().charAt(0);
+	        char computerTurn;
+	        if (userTurn == 'X' || userTurn == '0')
+	        {
+	            if (userTurn == userTurn) {
+	            	computerTurn = 'o';
+	                System.out.println("Player has chosen " + userTurn + " and computer is  "+computerTurn);
+	            } else {
+	            	computerTurn = 'x';
+	                System.out.println("Player has chosen " + userTurn + " and computer is "+computerTurn);
+	            }
+	        }
+	        else
+	        {
+	            System.out.println("Enter only X or O"+ "");
+	            userOption();
+	        }
 	    }
 	}
